48629d90f364a90197b3a72800fc21c3ebce587d
Prince Codes <princecodes247@gmail.com>
2022-05-21 08:59:32 -0400

    update waitlist page

--- pages/waitlist.js
+++ pages/waitlist.js
@@ -7,6 +7,7 @@ import SideMenu from '../components/SideMenu'
 import WaitlistForm from '../components/WaitlistForm/WaitlistForm'
 
 import styles from '../styles/Waitlist.module.css'
+import FeedbackSlider from '../components/FeedbackSlider/FeedbackSlider'
 
 
 export default function Waitlist() {
@@ -28,11 +29,19 @@ export default function Waitlist() {
           <WaitlistForm/>
         </section>
         <section>
+        Feedback
+        <FeedbackSlider/>
+        </section>
+        <section>
         What we do
         <p>
         With Veldora you can creae your form and send the forms data to our server. we handle the rest. from email notifications, to submission management and even data validation can be handled flawlessly without any backend from your end.
 validation can be handled flawlessly without any backend from your endvalidation can be handled flawlessly without any backend from your end.
         </p>
+        </section>
+        <section>
+          <h2>How it works</h2>
+          
         </section>
        <Footer/>
       </main>
