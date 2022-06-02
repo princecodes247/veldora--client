8cda057425468e03ec3a4c8b310647b501a5e0dc
Prince Codes <princecodes247@gmail.com>
2022-06-02 05:40:12 -0700

    style: update Header

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 //import { Test } from './Header.styles';
 
 const Header = (props) => (
-  <div className="HeaderWrapper">
+  <div className="flex items-center justify-between w-full mx-8">
     <div className="logo">
     Veldora
     </div>
