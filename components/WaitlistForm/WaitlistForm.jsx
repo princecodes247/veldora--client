dcc8d1fb9c4b9318bf7cf5db6235b062fc729969
Prince Codes <princecodes247@gmail.com>
2022-06-02 05:41:10 -0700

    style: update waitlist form

--- components/WaitlistForm/WaitlistForm.jsx
+++ components/WaitlistForm/WaitlistForm.jsx
@@ -3,8 +3,9 @@ import PropTypes from 'prop-types';
 //import { Test } from './WaitlistForm.styles';
 
 const WaitlistForm = (props) => (
-  <div className="WaitlistFormWrapper">
-    Test content
+  <div className="flex self-start border-2 border-white w-">
+    <input className='p-4' type="text" />
+    <button className='text-white bg-red-600'>Join</button>
   </div>
 );
 
