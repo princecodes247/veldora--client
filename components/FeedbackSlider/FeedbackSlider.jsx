0d97c7b902d042d2a8e84e2ef5fb3e5cd96679b2
Prince Codes <princecodes247@gmail.com>
2022-06-02 05:39:34 -0700

    Styles: update Feedback Slider

--- components/FeedbackSlider/FeedbackSlider.jsx
+++ components/FeedbackSlider/FeedbackSlider.jsx
@@ -5,11 +5,11 @@ import FeedbackCard from '../FeedbackCard/FeedbackCard';
 
 const FeedbackSlider = (props) => (
   <div className="FeedbackSliderWrapper">
-    <div className="slider">
+    <div className="relative flex items-center gap-3 pr-12 slider">
     <FeedbackCard/>
     <FeedbackCard/>
     <FeedbackCard/>
-    <div className="nextButton">
+    <div className="absolute right-0 w-10 h-10 border-2 border-orange-400 rounded-full nextButton">
       P
     </div>
     </div>
