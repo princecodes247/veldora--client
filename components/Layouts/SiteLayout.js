3e7c2f35803bfaadee703d0326201986d2d435df
Prince Codes <princecodes247@gmail.com>
2022-06-02 05:40:35 -0700

    feat: create SiteLayout

---
+++ components/Layouts/SiteLayout.js
@@ -0,0 +1,14 @@
+// components/layout.js
+
+import Header from '../Header'
+import Footer from '../Footer'
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Header />
+      <main>{children}</main>
+      <Footer />
+    </>
+  )
+}
