b8b6398d4f85d8d5feb77fa6fd35b1ee7f1f6341
Prince Codes <princecodes247@gmail.com>
2022-06-04 03:46:49 -0700

    update Footer

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -1,20 +1,43 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import Link from "next/link";
 //import { Test } from './Footer.styles';
 
 const Footer = (props) => (
-  <footer className="">
-  <a
-    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-    target="_blank"
-    rel="noopener noreferrer"
-  >
-    Poweredjjjja by{" "}
-    <span className="">
-      {/* <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} /> */}
-    </span>
-  </a>
-</footer>
+  <footer className="flex justify-between px-6 pt-16 pb-32 mt-12 mr-32 border-t border-yellow-500">
+    <ul className="flex flex-col gap-3">
+      <li>
+        <div className="logo"></div>
+      </li>
+      <Link href="/">
+        <li className="uppercase">Home</li>
+      </Link>
+      <Link href="/services">
+        <li className="uppercase">Services</li>
+      </Link>
+      <Link href="/about">
+        <li className="uppercase">About</li>
+      </Link>
+      <Link href="/contact">
+        <li className="uppercase">Contact</li>
+      </Link>
+    </ul>
+    <ul className="flex flex-col gap-2">
+      <h2 className="text-3xl">Contact Us</h2>
+      <li>Linkedin</li>
+      <li>Facebook</li>
+      <li>Instagram</li>
+    </ul>
+    <ul className="flex flex-col gap-1">
+      <h2 className="text-3xl">Misc</h2>
+      <li>Frequently Asked Questions</li>
+      <li>Documentation</li>
+      <li>Security Details</li>
+      <Link href="/signup">
+      <button className="p-6 py-2 mt-2 italic font-bold text-purple-800 bg-yellow-500">Get started</button>
+      </Link>
+    </ul>
+  </footer>
 );
 
 Footer.propTypes = {
