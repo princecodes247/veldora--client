dbe5352042a3f000a271104815a760c1c0629df9
Prince Codes <princecodes247@gmail.com>
2022-06-04 03:49:28 -0700

    update WaitlistForm styles

--- components/WaitlistForm/WaitlistForm.jsx
+++ components/WaitlistForm/WaitlistForm.jsx
@@ -3,10 +3,17 @@ import PropTypes from 'prop-types';
 //import { Test } from './WaitlistForm.styles';
 
 const WaitlistForm = (props) => (
-  <div className="flex self-start border-2 border-white w-">
-    <input className='p-4' type="text" />
-    <button className='text-white bg-red-600'>Join</button>
-  </div>
+ <React.Fragment>
+    {
+    props.alt ? <div className="flex items-stretch self-center pl-1 pr-0.5 bg-white border-2 border-white rounded focus-within:outline-solid-yellow-500 focus-within:border-yellow-400 w-96">
+    <input className='flex-1 p-3 text-black outline-none' type="text" placeholder='Type your email to join our wait list' />
+    <button className='py-2 text-xl font-bold text-white bg-red-600 rounded-lg px-7'>Join</button>
+  </div> : <div className="flex items-stretch self-start pl-1 p-0.5 bg-white border-2 border-white rounded focus-within:outline-solid-yellow-500 focus-within:border-yellow-400 w-96">
+  <input className='flex-1 p-4 text-black outline-none' type="text" placeholder='Type your email to join our wait list' />
+  <button className='py-2 text-xl font-bold text-white bg-red-600 rounded-lg px-7'>Join</button>
+</div>
+  }
+ </React.Fragment>
 );
 
 WaitlistForm.propTypes = {
