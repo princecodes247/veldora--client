f755952634445c05310d971a532973e9ae83b7cc
Prince Codes <princecodes247@gmail.com>
2022-06-04 03:47:45 -0700

    update SideMenu styles

--- components/SideMenu/SideMenu.jsx
+++ components/SideMenu/SideMenu.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 //import { Test } from './SideMenu.styles';
 
 const SideMenu = (props) => (
-  <aside className="SideMenuWrapper">
+  <aside className="flex-1 p-8 border-l-2 border-yellow-400">
     <div className="buttons">
       <button className="dno">
         Signin
