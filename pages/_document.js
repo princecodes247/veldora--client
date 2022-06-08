673e7a268cbe2035c178708964230962c844c54b
Prince Codes <princecodes247@gmail.com>
2022-06-08 13:45:51 -0700

    fix: stop bg flashing

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ class MyDocument extends Document {
         <Head>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&family=Syne:wght@400;600;700&display=swap" rel="stylesheet" /> 
         </Head>
-        <body className="font-body">
+        <body className="font-body bg-black">
           <Main />
           <NextScript />
         </body>
