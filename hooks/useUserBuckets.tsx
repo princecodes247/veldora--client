61638ad9795e1d229a4ef7fbdbdf62e81bb4d71c
Prince Codes <princecodes247@gmail.com>
2023-06-25 14:33:37 +0100

    update fetch user buckets

--- hooks/useUserBuckets.tsx
+++ hooks/useUserBuckets.tsx
@@ -1,7 +1,7 @@
 import { useInfiniteQuery } from "@tanstack/react-query";
-import { getBuckets } from "../services/BucketService";
+import { getBuckets, getUserBuckets } from "../services/BucketService";
 
-const useBuckets = ({
+const useUserBuckets = ({
   name = "",
   page = 1,
   pageSize = 10,
@@ -13,18 +13,18 @@ const useBuckets = ({
   return useInfiniteQuery({
     queryKey: ["buckets", name, page, pageSize],
     queryFn: async ({ pageParam = 1 }) => {
-      const result = await getBuckets({
+      const result = await getUserBuckets({
         name,
         page: pageParam,
         pageSize,
       });
       console.log({ result: result.data.data });
-      const events = result.data.data;
-      return events;
+      const buckets = result.data.data;
+      return buckets;
     },
     getNextPageParam: (lastPage, pages) =>
       (lastPage?.meta?.current_page ?? 1) + 1,
   });
 };
 
-export default useBuckets;
+export default useUserBuckets;
