b28f66e6df6b09c6e5ee86d29c9467b4fe7469b9
Prince Codes <princecodes247@gmail.com>
2023-06-26 15:53:23 +0100

    update dashboard layout

--- layouts/Dashboard.layout.tsx
+++ layouts/Dashboard.layout.tsx
@@ -8,6 +8,7 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
   return (
     <div>
       <DashboardNav />
+      {children}
     </div>
   );
 }
--- layouts/Settings.layout.tsx
+++ layouts/Settings.layout.tsx
@@ -33,39 +33,39 @@ interface SettingsLayoutProps {
 export default function SettingsLayout({ children }: SettingsLayoutProps) {
   return (
     <DashboardLayout>
-    <div className="p-8 md:p-16">
-      <div className="md:hidden">
-        <Image
-          src="/examples/forms-light.png"
-          width={1280}
-          height={791}
-          alt="Forms"
-          className="block dark:hidden"
-        />
-        <Image
-          src="/examples/forms-dark.png"
-          width={1280}
-          height={791}
-          alt="Forms"
-          className="hidden dark:block"
-        />
-      </div>
-      <div className="hidden space-y-6 p-10 pb-16 md:block">
-        <div className="space-y-0.5">
-          <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
-          <p className="text-muted-foreground">
-            Manage your account settings and set e-mail preferences.
-          </p>
+      <div className="p-8 md:p-16">
+        <div className="md:hidden">
+          <Image
+            src="/examples/forms-light.png"
+            width={1280}
+            height={791}
+            alt="Forms"
+            className="block dark:hidden"
+          />
+          <Image
+            src="/examples/forms-dark.png"
+            width={1280}
+            height={791}
+            alt="Forms"
+            className="hidden dark:block"
+          />
         </div>
-        <Separator className="my-6" />
-        <div className="flex flex-col gap-8 space-y-8 md:flex-row lg:flex-row lg:space-x-12 lg:space-y-0">
-          <aside className="">
-            <SidebarNav items={sidebarNavItems} />
-          </aside>
-          {children}
+        <div className="hidden space-y-6 p-10 pb-16 md:block">
+          <div className="space-y-0.5">
+            <h2 className="text-2xl font-bold tracking-tight">Settings</h2>
+            <p className="text-muted-foreground">
+              Manage your account settings and set e-mail preferences.
+            </p>
+          </div>
+          <Separator className="my-6" />
+          <div className="flex flex-col gap-8 space-y-8 md:flex-row lg:flex-row lg:space-x-12 lg:space-y-0">
+            <aside className="">
+              <SidebarNav items={sidebarNavItems} />
+            </aside>
+            {children}
+          </div>
         </div>
       </div>
-    </div>
     </DashboardLayout>
   );
 }
