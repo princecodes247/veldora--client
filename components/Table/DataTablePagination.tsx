cd87ad450871d003509a1410e7c02abb68a5c5fd
Prince Codes <princecodes247@gmail.com>
2023-06-28 08:59:34 +0100

    fix table responsiveness

--- components/Table/DataTablePagination.tsx
+++ components/Table/DataTablePagination.tsx
@@ -23,12 +23,12 @@ export function DataTablePagination<TData>({
   table,
 }: DataTablePaginationProps<TData>) {
   return (
-    <div className="flex items-center justify-between px-2">
+    <div className="flex flex-col gap-4 md:flex-row justify-between px-2">
       <div className="flex-1 text-sm text-muted-foreground">
         {table.getFilteredSelectedRowModel().rows.length} of{" "}
         {table.getFilteredRowModel().rows.length} row(s) selected.
       </div>
-      <div className="flex items-center space-x-6 lg:space-x-8">
+      <div className="flex flex-col gap-4 md:flex-row space-x-6 lg:space-x-8">
         <div className="flex items-center space-x-2">
           <p className="text-sm font-medium">Rows per page</p>
           <Select
@@ -49,7 +49,8 @@ export function DataTablePagination<TData>({
             </SelectContent>
           </Select>
         </div>
-        <div className="flex w-[100px] items-center justify-center text-sm font-medium">
+       <div className="flex flex-row">
+       <div className="flex w-[100px] items-center justify-center text-sm font-medium">
           Page {table.getState().pagination.pageIndex + 1} of{" "}
           {table.getPageCount()}
         </div>
@@ -91,6 +92,7 @@ export function DataTablePagination<TData>({
             <ChevronsRight className="h-4 w-4" />
           </Button>
         </div>
+       </div>
       </div>
     </div>
   );
