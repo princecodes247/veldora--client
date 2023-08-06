419757e6b3f838993679de31b8c45009462ab8cf
Prince Codes <princecodes247@gmail.com>
2023-08-06 16:50:38 +0100

    update

---
+++ components/APITokenInput.tsx
@@ -0,0 +1,28 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { FormDescription } from "./ui/form"
+import { useMutate } from "@/hooks/useMutate"
+import { regenerateAPIToken } from "@/services/BucketService"
+ 
+export function  APITokenInput({
+    token,
+    regenerateFunction,
+    isRegenerating,
+}: {
+    token: string
+    regenerateFunction: () => void
+    isRegenerating: boolean
+}) {
+
+  return (
+    <div>
+        <div className="flex w-full max-w-sm items-center space-x-2">
+      <Input type="text" placeholder="API Token" value={token} />
+      <Button onClick={regenerateFunction} disabled={isRegenerating}>Regenerate</Button>
+    </div>
+      <p className="text-xs mt-1 text-gray-500">
+                Previous api tokens will stop working after a regeneration.
+              </p>
+    </div>
+  )
+}
---
+++ components/BucketStructure.tsx
@@ -0,0 +1,103 @@
+// import { Icons } from "@/components/icons"
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Textarea } from "./ui/textarea";
+import { RadioGroup, RadioGroupItem } from "./ui/radio-group";
+import { useState } from "react";
+import { IBucketData } from "@/interfaces";
+import { useMutate } from "@/hooks/useMutate";
+import { updateBucket } from "@/services/BucketService";
+import DeleteBucketDialog from "./dialogs/DeleteBucket.dialog";
+
+export function BucketStructure({bucket}: {bucket?: IBucketData}) {
+    const [bucketName, setBucketName] = useState(bucket?.name ?? "")
+    const [bucketDescription, setBucketDescription] = useState(bucket?.description ?? "")
+    const [responseStyle, setResponseStyle] = useState(bucket?.responseStyle ?? "default")
+    const [customRedirect, setCustomRedirect] = useState(bucket?.customRedirect ?? "")
+
+    const updateBucketMutation = useMutate(updateBucket, {
+        loadingMessage: "Updating Bucket"
+    })
+
+    const handleSubmit = () => updateBucketMutation.mutate({id: bucket?._id ??"", bucketData: {
+        customRedirect,
+        description: bucketDescription,
+        name: bucketName,
+        responseStyle
+    }})
+  return (
+    <>
+    <Card className="max-w-[700px]">
+      <CardHeader className="space-y-1">
+        <CardTitle className="text-2xl">Bucket Structure</CardTitle>
+        <CardDescription>
+          You can change details about your form here.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="grid gap-4">
+        <div className="grid gap-2">
+          <Label htmlFor="text">Bucket Name</Label>
+          <Input id="text" value={bucketName} onChange={(e) => setBucketName(e.target.value)} type="text" placeholder="Bucket Name" />
+        </div>
+
+        <div className="grid gap-2">
+          <Label htmlFor="text">Bucket Description</Label>
+          <Textarea value={bucketDescription} onChange={(e) => setBucketDescription(e.target.value)} id="text" placeholder="Bucket Description" />
+        </div>
+
+        <div className="grid gap-2">
+          <Label htmlFor="text">Response Style</Label>
+          <RadioGroup className="py-4" value={responseStyle} onValueChange={(value: "default" | "json" | "params" | "custom") => setResponseStyle(value)}>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="default" id="default" />
+              <Label htmlFor="default">Default</Label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="json" id="json" />
+              <Label htmlFor="json">JSON</Label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="params" id="params" />
+              <Label htmlFor="params">Params</Label>
+            </div>
+            <div className="flex items-center space-x-2">
+              <RadioGroupItem value="custom" id="custom" />
+              <Label htmlFor="custom">Custom</Label>
+            </div>
+          </RadioGroup>
+        </div>
+       {responseStyle === "custom" && <div className="grid gap-2">
+          <Label htmlFor="text">Custom Redirect</Label>
+          <Input id="text" value={customRedirect} onChange={(e) => setCustomRedirect(e.target.value)}  type="text" placeholder="https://redirect.com" />
+        </div>}
+      </CardContent>
+      <CardFooter className="flex flex-col gap-2">
+        <Button onClick={handleSubmit} disabled={(responseStyle === "custom" && customRedirect.trim().length === 0) || bucketDescription.trim().length === 0 || bucketName.trim().length === 0} className="w-full">Update Bucket</Button>
+      </CardFooter>
+    </Card>
+    <Card className="max-w-[700px]">
+      <CardHeader className="space-y-1">
+        <CardTitle className="text-2xl">Delete Bucket</CardTitle>
+        <CardDescription>
+          Delete your form bucket
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="grid gap-4">
+    
+      </CardContent>
+      <CardFooter className="flex flex-col gap-2">
+        <DeleteBucketDialog id={bucket?._id ?? ""} name={bucket?.name ?? ""}/>
+      </CardFooter>
+    </Card>
+    </>
+  );
+}
