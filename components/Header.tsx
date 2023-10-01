cc1798322c7cf342697e2bb79324e39d18e8e853
Prince Codes <princecodes247@gmail.com>
2023-10-01 23:36:30 +0100

    fix tiny ui bugs

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,22 +6,23 @@ import Link from "next/link";
 
 export function Header() {
   return (
-    <header className="flex font-semibold justify-between px-2 sm:px-6 z-10 top-0 w-full sticky top-0 border-b border-transparent backdrop-blur-sm transition duration-200 ease-in-out border-white/10  bg-black/50 backdrop-blur-md">
+    <header className="sticky top-0 z-10 flex w-full justify-between border-b border-transparent border-white/10 bg-black/50 px-2 font-semibold backdrop-blur-md backdrop-blur-sm transition duration-200 ease-in-out sm:px-6">
       <div className="relative p-8 py-4 text-white">
-        <Link href="/" className="block w-8 sm:w-32 ">
-
-        <Logo variant="responsive" />
+        <Link href="/" className="block w-8 md:w-32 ">
+          <Logo variant="responsive" />
         </Link>
       </div>
       <div className="flex items-center justify-center">
         <Link href="/login">
-        <Button variant="outline" className="border bg-transparent text-white font-semibold px-6">Get Started
-        <ArrowRight size={15} className="-mr-2 ml-2"/>
-        </Button>
+          <Button
+            variant="outline"
+            className="border bg-transparent px-6 font-semibold text-white"
+          >
+            Get Started
+            <ArrowRight size={15} className="-mr-2 ml-2" />
+          </Button>
         </Link>
       </div>
     </header>
   );
 }
-
-
