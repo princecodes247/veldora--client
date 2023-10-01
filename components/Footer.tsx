eeb3c4058ba2b144d97b87c92b7ac26e55651898
Prince Codes <princecodes247@gmail.com>
2023-10-01 23:39:30 +0100

    fix(build): remove '

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -6,7 +6,7 @@ import { appData } from "@/constants";
 export function Footer() {
   return (
     <>
-      <footer className="relative flex flex-col items-center gap-4 border-t border-gray-900 bg-transparent p-6 py-14 backdrop-blur-sm md:p-16 md:pb-8">
+      <footer className="relative flex flex-col items-center gap-4 p-6 bg-transparent border-t border-gray-900 py-14 backdrop-blur-sm md:p-16 md:pb-8">
         <div className="w-24">
           <Logo variant="base" />
         </div>
@@ -15,7 +15,7 @@ export function Footer() {
           href="https://www.github.com/princecodes247"
           className="text-sm text-gray-600 hover:text-white hover:underline"
         >
-          <p className="">Made with @princecodes247's Special Sauce</p>
+          <p className="">Made with @princecodes247&lsquo;s Special Sauce</p>
         </Link>
       </footer>
     </>
